616d4cc70ef0f149d0afa3de6000686c977b42d9
Patymaster <62792625+Patymaster@users.noreply.github.com>
2022-09-08 15:57:04 +0300

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LABA
+{
+  class User
+    {
+        public string login, name, lastname;
+        public int date, age;
+    }
+    class Program
+    {
+        static User GetUser()
+        {
+            var user = new User();
+            user.login = "Superman228";
+            user.name = "Ivan";
+            user.lastname = "Shevchenko";
+            user.age = 18;
+            user.date = 2022;
+            return user;
+        }
+        static void Print (User user)
+        {
+            Console.WriteLine("iнформацiя про користувача: ");
+            Console.WriteLine($"Login: {user.login}");
+            Console.WriteLine($"Name: {user.name}");
+            Console.WriteLine($"LastName: {user.lastname}");
+            Console.WriteLine($"Age: {user.age}");
+            Console.WriteLine($"Date of Registration: {user.date}");
+        }
+        static void Main(string[] args)
+        {
+            var info = GetUser();
+            Print(info);
+        }
+    }
+}
