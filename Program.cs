11232ba1a45a5ee69dc4e1a8a7d62e4add17cb00
Patymaster <62792625+Patymaster@users.noreply.github.com>
2022-09-08 15:48:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Zadacha1
+{
+    class Address
+    {
+        private int index = 07700;
+        private string country = "Ukraine", city = "Yagotyn", street = "Shevchenka", house = "26", apartment = "1";
+        public void address()
+        {
+            Console.WriteLine("Address: ");
+            Console.WriteLine($"index: {index}");
+            Console.WriteLine($"country: {country}");
+            Console.WriteLine($"city: {city}");
+            Console.WriteLine($"street: {street}");
+            Console.WriteLine($"house: {house}");
+            Console.WriteLine($"apartment: {apartment}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address A = new Address();
+            A.address();
+        }
+    }
+
+}
